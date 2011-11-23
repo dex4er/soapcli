6b2ea117dc23be57638ca92ca504d90c3c1a721b
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2011-11-23 13:07:59 +0100

    Module distribution

---
+++ lib/App/soapcli.pm
@@ -0,0 +1,39 @@
+package App::soapcli;
+
+=head1 NAME
+
+App::soapcli - Skeleton for new modules
+
+=head1 SYNOPSIS
+
+This is a package with sopacli(1) utility.
+
+=cut
+
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+
+
+=head1 SEE ALSO
+
+L<http://github.com/dex4er/soapcli>, soapcli(1).
+
+=head1 AUTHOR
+
+Piotr Roszatycki <dexter@cpan.org>
+
+=head1 LICENSE
+
+Copyright (c) 2011 Piotr Roszatycki <dexter@cpan.org>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+See L<http://dev.perl.org/licenses/artistic.html>
