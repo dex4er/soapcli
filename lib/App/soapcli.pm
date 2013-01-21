668f6899adc0fa367f67694f2502c5a4f6064673
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-01-21 12:47:49 +0100

    Bump version

--- lib/App/soapcli.pm
+++ lib/App/soapcli.pm
@@ -23,7 +23,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.0201';
 
 use Log::Report 'soapcli', syntax => 'SHORT';
 
--- script/soapcli.pl
+++ script/soapcli.pl
@@ -57,7 +57,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.0201';
 
 require App::soapcli;
 
